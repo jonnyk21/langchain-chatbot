48025706705724a16bcb53528f304b1184a67cd9
Jonathan <jkresselulm@gmail.com>
2024-12-09 00:01:48 +0100

    Fix: Update Chroma import to use langchain_community.vectorstores

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader, PDFMinerLoader, BSHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 import time
 import requests
